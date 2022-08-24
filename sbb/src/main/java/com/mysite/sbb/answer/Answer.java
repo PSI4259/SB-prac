1f6f827d5c1cbcd4d99372ce4d1a2798d935fe17
PARKSAGNIK <psi4259@gmail.com>
2022-08-24 15:23:28 +0900

    2022.08.24

---
+++ sbb/src/main/java/com/mysite/sbb/answer/Answer.java
@@ -0,0 +1,31 @@
+package com.mysite.sbb.answer;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.mysite.sbb.question.Question;
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.ManyToOne;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
